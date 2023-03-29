b1b28df86d33b9a9b9a587b0326758a854fe4b7d
Umar Shaik <ufshaikc@gmail.com>
2023-03-29 18:12:11 -0230

    Initial Project Upload

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "replicate.com",
+      "replicate.delivery",
+      "user-images.githubusercontent.com",
+      "upcdn.io"
+    ],
+  }
+};
+
+module.exports = nextConfig;
---
+++ pages/index.js
@@ -0,0 +1,123 @@
+import Canvas from "components/canvas";
+import PromptForm from "components/prompt-form";
+import Head from "next/head";
+import { useState } from "react";
+import Predictions from "components/predictions";
+import Error from "components/error";
+import uploadFile from "lib/upload";
+import Script from "next/script";
+import seeds from "lib/seeds";
+import pkg from "../package.json";
+import sleep from "lib/sleep";
+
+export default function Home() {
+  const [error, setError] = useState(null);
+  const [submissionCount, setSubmissionCount] = useState(0);
+  const [predictions, setPredictions] = useState({});
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [scribbleExists, setScribbleExists] = useState(false);
+  const [seed] = useState(seeds[Math.floor(Math.random() * seeds.length)]);
+  const [initialPrompt] = useState(seed.prompt);
+  const [scribble, setScribble] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // track submissions so we can show a spinner while waiting for the next prediction to be created
+    setSubmissionCount(submissionCount + 1);
+
+    const prompt = e.target.prompt.value;
+
+    setError(null);
+    setIsProcessing(true);
+
+    const fileUrl = await uploadFile(scribble);
+
+    const body = {
+      prompt,
+      image: fileUrl,
+    };
+
+    const response = await fetch("/api/predictions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    let prediction = await response.json();
+
+    setPredictions((predictions) => ({
+      ...predictions,
+      [prediction.id]: prediction,
+    }));
+
+    if (response.status !== 201) {
+      setError(prediction.detail);
+      return;
+    }
+
+    while (
+      prediction.status !== "succeeded" &&
+      prediction.status !== "failed"
+    ) {
+      await sleep(500);
+      const response = await fetch("/api/predictions/" + prediction.id);
+      prediction = await response.json();
+      setPredictions((predictions) => ({
+        ...predictions,
+        [prediction.id]: prediction,
+      }));
+      if (response.status !== 200) {
+        setError(prediction.detail);
+        return;
+      }
+    }
+
+    setIsProcessing(false);
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>{pkg.appName}</title>s
+      </Head>
+      <main className="container max-w-[1024px] mx-auto p-5 ">
+        <div className="container max-w-[512px] mx-auto">
+          <hgroup>
+            <h1 className="text-center text-5xl font-bold m-4">
+              {pkg.appName}
+            </h1>
+            <p className="text-center text-xl opacity-60 m-4">
+              {pkg.appSubtitle}
+            </p>
+          </hgroup>
+
+          <Canvas
+            startingPaths={seed.paths}
+            onScribble={setScribble}
+            scribbleExists={scribbleExists}
+            setScribbleExists={setScribbleExists}
+          />
+
+          <PromptForm
+            initialPrompt={initialPrompt}
+            onSubmit={handleSubmit}
+            isProcessing={isProcessing}
+            scribbleExists={scribbleExists}
+          />
+
+          <Error error={error} />
+        </div>
+
+        <Predictions
+          predictions={predictions}
+          isProcessing={isProcessing}
+          submissionCount={submissionCount}
+        />
+      </main>
+
+      <Script src="https://js.upload.io/upload-js-full/v1" />
+    </div>
+  );
+}
